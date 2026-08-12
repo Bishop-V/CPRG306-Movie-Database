b7257e77e245b39f1d41365426ee82c5fddb9217
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-08-11 18:08:39 -0600

    adding movie function

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
 import { saveMovie } from "@/lib/actions";
 
 export default function MovieForm() {
   const [state, formAction] = useActionState(saveMovie.bind(null, null), {});
-
+  const {title, setTitle} = useState("");
+  const {year, setYear} = useState("");
+  const {actors, setActors} = useState("");
   const inputStyle =
     "border-2 border-purple-200 rounded-2xl placeholder:text-gray-400 text-center";
 
@@ -15,23 +17,36 @@ export default function MovieForm() {
         <h2 className="text-purple-400 text-3xl font-bold">Add a movie</h2>
 
         <form action={formAction} className="mt-7 gap-6 flex flex-col">
-          <input className={inputStyle} name="title" placeholder=" Title" />
+          <input className={inputStyle} 
+          value = {title}
+          onChange = {(e)=> setTitle(e.target.value)}
+          name="title" 
+          placeholder=" Title" />
+
           <input
             className={inputStyle}
+            value={year}
+            onChange ={(e)=> setYear(e.target.value)}
             name="release_year"
             placeholder=" Year"
           />
-          <input className={inputStyle} name="actors" placeholder=" Actors" />
+
+          <input className={inputStyle} 
+          value={actors}
+          onChange = {(e)=> setActors(e.target.value)}
+          name="actors" 
+          placeholder=" Actors" />
 
           {state.error && (
             <p className="text-red-300 text-center">{state.error}</p>
           )}
 
-          <input
+          <button
             type="submit"
-            value="Submit"
             className="bg-purple-300 px-2 py-1 rounded-lg hover:bg-purple-200 cursor-pointer"
-          />
+          >
+            Submit
+          </button>
         </form>
       </div>
     </main>
